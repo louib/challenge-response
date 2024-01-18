91204ddd83463e9d88292a6afe25eb357f23924f
Ashutosh Verma <github@ashu.io>
2024-01-19 00:54:23 +0530

    feat: multiple key support
    BREAKING CHANGE: Rename Symbols and add new fields and methods to device struct

--- examples/challenge_response_otp.rs
+++ examples/challenge_response_otp.rs
@@ -13,11 +13,7 @@ fn main() {
             device.vendor_id, device.product_id
         );
 
-        let config = Config::default()
-            .set_vendor_id(device.vendor_id)
-            .set_product_id(device.product_id)
-            .set_mode(Mode::Otp)
-            .set_slot(Slot::Slot2);
+        let config = Config::new_from(device).set_mode(Mode::Otp).set_slot(Slot::Slot2);
 
         // Challenge can not be greater than 64 bytes
         let challenge: &[u8] = b"my_challenge";
