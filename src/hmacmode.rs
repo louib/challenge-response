d7d92b60e34af88de67bdcc1a49526de1665c818
louib <code@louib.net>
2024-02-24 14:29:05 -0500

    refactor: extract hmac secret length

--- src/hmacmode.rs
+++ src/hmacmode.rs
@@ -2,8 +2,15 @@ use rand::Rng;
 use sec::hmac_sha1;
 use std;
 
+/// Size of the secret used by the HMAC algorithm
+pub const HMAC_SECRET_SIZE: usize = 20;
+
+/// Secret used to seed the HMAC algorithm
+pub type HmacSecret = [u8; HMAC_SECRET_SIZE];
+
 #[derive(Debug)]
-pub struct Hmac(pub [u8; 20]);
+pub struct Hmac(pub HmacSecret);
+
 impl Drop for Hmac {
     fn drop(&mut self) {
         for i in self.0.iter_mut() {
@@ -25,9 +32,9 @@ impl Hmac {
     }
 }
 
-/// A secret key for HMAC.
+/// A secret key for HMAC, derived from the HMAC secret
 #[derive(Debug)]
-pub struct HmacKey(pub [u8; 20]);
+pub struct HmacKey(pub HmacSecret);
 impl Drop for HmacKey {
     fn drop(&mut self) {
         for i in self.0.iter_mut() {
@@ -38,13 +45,13 @@ impl Drop for HmacKey {
 
 impl HmacKey {
     pub fn from_slice(s: &[u8]) -> Self {
-        let mut key = HmacKey([0; 20]);
+        let mut key = HmacKey([0; HMAC_SECRET_SIZE]);
         (&mut key.0).clone_from_slice(s);
         key
     }
 
     pub fn generate<R: Rng>(mut rng: R) -> Self {
-        let mut key = HmacKey([0; 20]);
+        let mut key = HmacKey([0; HMAC_SECRET_SIZE]);
         for i in key.0.iter_mut() {
             *i = rng.gen()
         }
