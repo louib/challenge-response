9090a69dcb50ea879e50ecca608ad77db5e9297b
louib <code@louib.net>
2024-09-08 19:07:14 -0400

    refactor: create a usb backend trait (#57)

--- src/usb.rs
+++ src/usb.rs
@@ -1,7 +1,3 @@
-#[cfg(all(feature = "nusb", not(feature = "rusb")))]
-use nusb::Device;
-#[cfg(feature = "rusb")]
-use rusb::{Context as RUSBContext, DeviceHandle as RUSBDeviceHandle};
 use std::time::Duration;
 use std::{slice, thread};
 
@@ -9,20 +5,32 @@ use config::Command;
 use error::ChallengeResponseError;
 use sec::crc16;
 
-#[cfg(all(feature = "nusb", not(feature = "rusb")))]
-mod nusb;
 #[cfg(feature = "rusb")]
-mod rusb;
+pub type BackendType = rusb::RUSBBackend;
+#[cfg(all(feature = "nusb", not(feature = "rusb")))]
+pub type BackendType = nusb::NUSBBackend;
+
+/// If using a variable-length challenge, the challenge must be stricly smaller than this value.
+/// If using a fixed-length challenge, the challenge must be exactly equal to this value.
+pub const CHALLENGE_SIZE: usize = 64;
+
+const VENDOR_ID: [u16; 3] = [
+    0x1050, // Yubico ( Yubikeys )
+    0x1D50, // OpenMoko ( Onlykey )
+    0x20A0, // Flirc ( Nitrokey )
+];
+const PRODUCT_ID: [u16; 11] = [
+    0x0010, // YubiKey Gen 1 & 2
+    0x0110, 0x0113, 0x0114, 0x0116, // YubiKey NEO
+    0x0401, 0x0403, 0x0405, 0x0407, // Yubikey 4 & 5
+    0x60FC, // Onlykey
+    0x4211, // NitroKey
+];
 
 #[cfg(all(feature = "nusb", not(feature = "rusb")))]
-use usb::nusb::{raw_write, read};
+pub mod nusb;
 #[cfg(feature = "rusb")]
-use usb::rusb::{raw_write, read};
-
-#[cfg(all(feature = "nusb", not(feature = "rusb")))]
-pub use usb::nusb::{close_device, open_device};
-#[cfg(feature = "rusb")]
-pub use usb::rusb::{close_device, open_device};
+pub mod rusb;
 
 /// The size of the payload when writing a request to the usb interface.
 pub(crate) const PAYLOAD_SIZE: usize = 64;
@@ -66,90 +74,120 @@ impl Frame {
     }
 }
 
-#[cfg(feature = "rusb")]
-pub type Context = RUSBContext;
-#[cfg(all(feature = "nusb", not(feature = "rusb")))]
-pub type Context = ();
+#[derive(Clone, Debug, PartialEq)]
+pub struct Device {
+    pub name: Option<String>,
+    pub serial: Option<u32>,
+    pub product_id: u16,
+    pub vendor_id: u16,
+    pub bus_id: u8,
+    pub address_id: u8,
+}
 
-#[cfg(feature = "rusb")]
-pub(crate) type DeviceHandle = RUSBDeviceHandle<Context>;
-#[cfg(all(feature = "nusb", not(feature = "rusb")))]
-pub(crate) type DeviceHandle = Device;
+pub trait Backend<DeviceHandle, Interface> {
+    fn new() -> Result<Self, ChallengeResponseError>
+    where
+        Self: Sized;
 
-pub fn write_frame(handle: &mut DeviceHandle, frame: &Frame) -> Result<(), ChallengeResponseError> {
-    let mut data = unsafe { slice::from_raw_parts(frame as *const Frame as *const u8, 70) };
+    fn open_device(
+        &mut self,
+        bus_id: u8,
+        address_id: u8,
+    ) -> Result<(DeviceHandle, Vec<Interface>), ChallengeResponseError>;
 
-    let mut seq = 0;
-    let mut buf = [0; 8];
-    while !data.is_empty() {
-        let (a, b) = data.split_at(7);
+    fn close_device(
+        &self,
+        handle: DeviceHandle,
+        interfaces: Vec<Interface>,
+    ) -> Result<(), ChallengeResponseError>;
 
-        if seq == 0 || b.is_empty() || a.iter().any(|&x| x != 0) {
-            let mut packet = [0; 8];
-            (&mut packet[..7]).copy_from_slice(a);
+    fn read(&self, handle: &mut DeviceHandle, buf: &mut [u8]) -> Result<usize, ChallengeResponseError>;
+    fn raw_write(&self, handle: &mut DeviceHandle, packet: &[u8]) -> Result<(), ChallengeResponseError>;
 
-            packet[7] = Flags::SLOT_WRITE_FLAG.bits() + seq;
-            wait(handle, |x| !x.contains(Flags::SLOT_WRITE_FLAG), &mut buf)?;
-            raw_write(handle, &packet)?;
+    fn find_device(&mut self) -> Result<Device, ChallengeResponseError>;
+    fn find_device_from_serial(&mut self, serial: u32) -> Result<Device, ChallengeResponseError>;
+    fn find_all_devices(&mut self) -> Result<Vec<Device>, ChallengeResponseError>;
+
+    fn write_frame(&self, handle: &mut DeviceHandle, frame: &Frame) -> Result<(), ChallengeResponseError> {
+        let mut data = unsafe { slice::from_raw_parts(frame as *const Frame as *const u8, 70) };
+
+        let mut seq = 0;
+        let mut buf = [0; 8];
+        while !data.is_empty() {
+            let (a, b) = data.split_at(7);
+
+            if seq == 0 || b.is_empty() || a.iter().any(|&x| x != 0) {
+                let mut packet = [0; 8];
+                (&mut packet[..7]).copy_from_slice(a);
+
+                packet[7] = Flags::SLOT_WRITE_FLAG.bits() + seq;
+                self.wait(handle, |x| !x.contains(Flags::SLOT_WRITE_FLAG), &mut buf)?;
+                self.raw_write(handle, &packet)?;
+            }
+            data = b;
+            seq += 1
         }
-        data = b;
-        seq += 1
+        Ok(())
     }
-    Ok(())
-}
 
-pub fn wait<F: Fn(Flags) -> bool>(
-    handle: &mut DeviceHandle,
-    f: F,
-    buf: &mut [u8],
-) -> Result<(), ChallengeResponseError> {
-    loop {
-        read(handle, buf)?;
-        let flags = Flags::from_bits_truncate(buf[7]);
-        if flags.contains(Flags::SLOT_WRITE_FLAG) || flags.is_empty() {
-            // Should store the version
-        }
+    fn wait<F: Fn(Flags) -> bool>(
+        &self,
+        handle: &mut DeviceHandle,
+        f: F,
+        buf: &mut [u8],
+    ) -> Result<(), ChallengeResponseError> {
+        loop {
+            self.read(handle, buf)?;
+            let flags = Flags::from_bits_truncate(buf[7]);
+            if flags.contains(Flags::SLOT_WRITE_FLAG) || flags.is_empty() {
+                // Should store the version
+            }
 
-        if f(flags) {
-            return Ok(());
+            if f(flags) {
+                return Ok(());
+            }
+            thread::sleep(Duration::new(0, 1000000));
         }
-        thread::sleep(Duration::new(0, 1000000));
     }
-}
 
-/// Reset the write state after a read.
-pub fn write_reset(handle: &mut DeviceHandle) -> Result<(), ChallengeResponseError> {
-    raw_write(handle, &WRITE_RESET_PAYLOAD)?;
-    let mut buf = [0; 8];
-    wait(handle, |x| !x.contains(Flags::SLOT_WRITE_FLAG), &mut buf)?;
-    Ok(())
-}
+    /// Reset the write state after a read.
+    fn write_reset(&self, handle: &mut DeviceHandle) -> Result<(), ChallengeResponseError> {
+        self.raw_write(handle, &WRITE_RESET_PAYLOAD)?;
+        let mut buf = [0; 8];
+        self.wait(handle, |x| !x.contains(Flags::SLOT_WRITE_FLAG), &mut buf)?;
+        Ok(())
+    }
 
-pub fn read_response(handle: &mut DeviceHandle, response: &mut [u8]) -> Result<usize, ChallengeResponseError> {
-    let mut r0 = 0;
-    wait(
-        handle,
-        |f| f.contains(Flags::RESP_PENDING_FLAG),
-        &mut response[..8],
-    )?;
-    r0 += 7;
-    loop {
-        if read(handle, &mut response[r0..r0 + 8])? < 8 {
-            break;
-        }
-        let flags = Flags::from_bits_truncate(response[r0 + 7]);
-        if flags.contains(Flags::RESP_PENDING_FLAG) {
-            let seq = response[r0 + 7] & 0b00011111;
-            if r0 > 0 && seq == 0 {
-                // If the sequence number is 0, and we have read at
-                // least one packet, stop.
+    fn read_response(
+        &self,
+        handle: &mut DeviceHandle,
+        response: &mut [u8],
+    ) -> Result<usize, ChallengeResponseError> {
+        let mut r0 = 0;
+        self.wait(
+            handle,
+            |f| f.contains(Flags::RESP_PENDING_FLAG),
+            &mut response[..8],
+        )?;
+        r0 += 7;
+        loop {
+            if self.read(handle, &mut response[r0..r0 + 8])? < 8 {
                 break;
             }
-        } else {
-            break;
+            let flags = Flags::from_bits_truncate(response[r0 + 7]);
+            if flags.contains(Flags::RESP_PENDING_FLAG) {
+                let seq = response[r0 + 7] & 0b00011111;
+                if r0 > 0 && seq == 0 {
+                    // If the sequence number is 0, and we have read at
+                    // least one packet, stop.
+                    break;
+                }
+            } else {
+                break;
+            }
+            r0 += 7;
         }
-        r0 += 7;
+        self.write_reset(handle)?;
+        Ok(r0)
     }
-    write_reset(handle)?;
-    Ok(r0)
 }
